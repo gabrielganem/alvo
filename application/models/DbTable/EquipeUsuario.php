eb32662b27b88046f3f71e499b92f4be19683cd9
gabrielganem <j.gabrielgb@gmail.com>
2014-05-23 14:48:48 -0300

    first commit

---
+++ application/models/DbTable/EquipeUsuario.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_EquipeUsuario extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'equipe_usuario';
+
+
+}
+
---
+++ application/views/scripts/index/login.js
@@ -0,0 +1,77 @@
+var canvas;
+var centerX = 300;
+var centerY = 300;
+var context;
+var angulo_p = 0;
+var angulo_c = 0;
+var raio = 15;
+            
+function contorno() {
+    var radius = 200;
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, angulo_c * (Math.PI / 180), false);
+    context.fillStyle = '#A73339';
+              //context.fill();
+    context.lineWidth = 20;
+    context.strokeStyle = '#A73339';
+    context.stroke();
+}
+    
+function background(raio) {
+    var radius = raio;
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = '#FFF';
+    context.fill();
+    context.lineWidth = 20;
+    context.strokeStyle = '#A73339';
+    //context.stroke();
+}
+
+function progresso(angulo_p) {
+    var radius = 220;
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, angulo_p * (Math.PI / 180), false);
+    context.fillStyle = '#FFF';
+    //context.fill();
+    context.lineWidth = 20;
+    context.strokeStyle = '#FFF';
+    context.stroke();
+}
+
+function draw_progress() {
+    progresso(angulo_p);
+    if (angulo_p < 360) {
+        angulo_p = angulo_p + 3;
+    }
+}
+
+function progresso_login() {
+    return setInterval(draw_progress,3);
+}
+
+function draw() {
+    background(raio);
+    if (angulo_c == 360) {
+        document.getElementById("form_login").style.display = "block";
+    }
+    if (raio < 200) {
+        raio = raio + 2;
+    }
+    if (raio == 201) {
+        contorno(angulo_c);
+        if (angulo_c < 360) {
+            angulo_c = angulo_c + 2;
+        }
+        
+    }
+}
+
+function init() {
+    document.getElementById("hoverme").style.display = "none";
+    canvas = document.getElementById("circle");
+    context = canvas.getContext('2d');
+    return setInterval(draw, 3);
+}
+
+init();
